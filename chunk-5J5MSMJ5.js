6041300a7b8cce9b8ea9d701ddcbfe19c5c74e0f
mostafa-hessen <mb4952144@gmail.com>
2024-10-02 14:43:13 +0300

    F

---
+++ chunk-5J5MSMJ5.js
@@ -0,0 +1,1 @@
+import{Ga as n,Ha as a,Na as p,Pa as s,W as o}from"./chunk-7B4FCOO4.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-about"]],standalone:!0,features:[s],decls:2,vars:0,template:function(e,r){e&1&&(n(0,"p"),p(1,"about works!"),a())}})};export{i as AboutComponent};
